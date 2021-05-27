1df2a19d3f83f5353f88e150da597b214fab9feb
MaxPopsuy <maim09p@gmail.com>
2021-05-27 19:51:44 +0300

    Game Page almost done Js

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,70 @@
-import images from "./images.js";
+const cards = document.querySelectorAll('.card');
 
-// modal windows
-
-
-
-
-
-
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard;
+let secondCard;
+let counter = 0;
 
 
+const flipCard = function() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
 
-// other js
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    firstCard.style.cursor = "initial";
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  secondCard.style.cursor = "initial";
+  checkForMatch();
+}
+
+const checkForMatch = function() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+const disableCards = function() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  firstCard.style.cursor = "initial";
+  secondCard.style.cursor = "initial";
+  counter++;
+  resetBoard();
+}
+
+const unflipCards = function() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    firstCard.style.cursor = "pointer";
+    secondCard.style.cursor = "pointer";
+    resetBoard();
+  }, 500);
+}
+
+const resetBoard = function() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+;
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 15);
+    card.style.order = randomPosition;
+  });
+}());
+
+cards.forEach(card => card.addEventListener('click', flipCard));
---
+++ app/js/modal.js
@@ -0,0 +1,30 @@
+const startModalRef = document.querySelector(".main__start__modal-window");
+const startModalBtn = document.querySelector(".start_game");
+const audioRef = document.querySelector(".bg-music");
+// audioRef.play();
+
+
+const openStartModal = ()=>{
+    window.addEventListener('keydown', onPressEscape)
+    startModalRef.classList.add('show_modal');
+}
+
+const onCloseModal = function() {
+    window.removeEventListener('keydown', onPressEscape);
+    startModalRef.classList.remove('show_modal');
+};
+
+const onBackdropClick = function(event) {
+    if(event.target === event.currentTarget) {
+        onCloseModal();
+    }
+};
+
+function onPressEscape(event){
+    if(event.code === 'Escape'){
+        onCloseModal();
+    }
+};
+
+startModalBtn.addEventListener("click", openStartModal);
+startModalRef.addEventListener('click', onBackdropClick);
